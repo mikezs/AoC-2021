91b3488d7c25350bb0b82d0bd627c505501c53ff
Mike <1911467+mikezs@users.noreply.github.com>
2021-11-30 21:11:47 +0000

    More build updates and exclude secret files from build to silence warning

--- Package.swift
+++ Package.swift
@@ -6,16 +6,12 @@ import PackageDescription
 let package = Package(
     name: "AoC",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this
-        //  package depends on.
         .target(
             name: "AoC",
-            dependencies: ["AoCKit"]),
+            dependencies: ["AoCKit"],
+            exclude: ["config.swift.secret"]),
         .target(name: "AoCKit"),
         .testTarget(
             name: "AoCKitTests",
