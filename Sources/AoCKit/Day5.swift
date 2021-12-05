6abcba284ce6c56039c4581b97ab8b6c142c56cd
Mike <1911467+mikezs@users.noreply.github.com>
2021-12-05 11:54:56 +0000

    Day 5

--- Sources/AoCKit/Day5.swift
+++ Sources/AoCKit/Day5.swift
@@ -1,17 +1,86 @@
 import Foundation
 
 public final class Day5: Day {
-    let input: [Int]
+    typealias Line = ((x: Int, y: Int),(x: Int, y: Int))
+    let input: [Line]
 
     public init(input: String) {
-        self.input = input.trimmedLines.compactMap { Int($0) }
+        self.input = input.trimmedLines.compactMap {
+            let parts = $0.split(separator: " ")
+            let start = parts[0].split(separator: ",")
+            let end = parts[2].split(separator: ",")
+            
+            return ((Int(start[0])!, Int(start[1])!), (Int(end[0])!, Int(end[1])!))
+        }
     }
 
     public func part1() -> Int {
-        return 0
+        let lines = input.filter {
+            $0.0.x == $0.1.x || $0.0.y == $0.1.y
+        }
+        
+        var maxX = 0
+        var maxY = 0
+        
+        for line in lines {
+            maxX = max(max(line.0.x, line.1.x) , maxX)
+            maxY = max(max(line.0.y, line.1.y) , maxY)
+        }
+        
+        var grid = [[Int]](repeating: [Int](repeating: 0, count: maxX + 1), count: maxY + 1)
+        
+        for line in lines {
+            if line.0.x == line.1.x {
+                for y in (min(line.0.y, line.1.y) ... max(line.0.y, line.1.y)) {
+                    grid[y][line.0.x] += 1
+                }
+            } else if line.0.y == line.1.y {
+                for x in (min(line.0.x, line.1.x) ... max(line.0.x, line.1.x)) {
+                    grid[line.0.y][x] += 1
+                }
+            }
+        }
+        
+//        for y in grid {
+//            print(y.map { String($0) }.joined())
+//        }
+        
+        return grid
+            .flatMap { $0 }
+            .filter { $0 > 1 }
+            .count
     }
 
     public func part2() -> Int {
-        return 0
+        var maxX = 0
+        var maxY = 0
+        
+        for line in input {
+            maxX = max(max(line.0.x, line.1.x) , maxX)
+            maxY = max(max(line.0.y, line.1.y) , maxY)
+        }
+        
+        var grid = [[Int]](repeating: [Int](repeating: 0, count: maxX + 1), count: maxY + 1)
+        
+        for line in input {
+            let xDirection = (line.1.x - line.0.x).signum()
+            let yDirection = (line.1.y - line.0.y).signum()
+            
+            let xSequence = xDirection == 0 ? [Int](repeating: line.0.x, count: abs(line.0.y - line.1.y) + 1) : Array(stride(from: line.0.x, through: line.1.x, by: xDirection))
+            let ySequence = yDirection == 0 ? [Int](repeating: line.0.y, count: abs(line.0.x - line.1.x) + 1) : Array(stride(from: line.0.y, through: line.1.y, by: yDirection))
+            
+            for (x,y) in zip(xSequence, ySequence) {
+                grid[y][x] += 1
+            }
+        }
+        
+//        for y in grid {
+//            print(y.map { String($0).replacingOccurrences(of: "0", with: ".") }.joined())
+//        }
+        
+        return grid
+            .flatMap { $0 }
+            .filter { $0 > 1 }
+            .count
     }
 }
--- Tests/AoCKitTests/AoC_Tests.swift
+++ Tests/AoCKitTests/AoC_Tests.swift
@@ -142,14 +142,24 @@ forward 2
 
 // MARK: - Day 5
     let day5Input = """
+0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
 """
 
     func testDay5Part1() {
-        XCTAssertEqual(Day5(input: day5Input).part1(), 0)
+        XCTAssertEqual(Day5(input: day5Input).part1(), 5)
     }
 
     func testDay5Part2() {
-        XCTAssertEqual(Day5(input: day5Input).part2(), 0)
+        XCTAssertEqual(Day5(input: day5Input).part2(), 12)
     }
 
     // MARK: - Day 6
